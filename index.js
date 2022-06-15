f3d8c60a25405c91e1aa4bbbc7507fbea2223649
Emanuel Hitallo <100162081+Emanuel0001@users.noreply.github.com>
2022-06-15 16:19:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ const app = express();
 let user = [];
 
 app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 
 const { Client } = require('pg');
